46cde455e66b786b2e037134ec68a471fb6937f1
HBK <sammcowen@gmail.com>
2022-03-07 22:24:49 -0800

    created query to get rows from db

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+const mysql = require('mysql2'); 
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,8 +8,20 @@ const app  = express();
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+    //  connect to database
+    const db = mysql.createConnection(
+        {
+            host:'localhost', 
+            user: 'root',
+            password:'password',
+            database:'election'
+        },
+        console.log('Connected to election database')
+    )
 
-
+db.query(`SELECT * FROM candidates`, (err,rows) => {
+    console.log(rows);
+})
 app.use((req,res) => {
     res.status(404).end();
 });
