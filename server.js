c27506d454b32ebf6adbb672197e1f9823b70b96
HBK <sammcowen@gmail.com>
2022-03-09 21:54:22 -0800

    updated candidate routes to join with party data

--- server.js
+++ server.js
@@ -26,7 +26,11 @@ const db = mysql.createConnection(
 );
 // query for all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
     db.query(sql, (err, rows) => {
         if (err) {
             res.status(500).json({ error: err.message });
@@ -41,7 +45,12 @@ app.get('/api/candidates', (req, res) => {
 // query for single candidate by id
 // Get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name 
+  AS party_name 
+  FROM candidates 
+  LEFT JOIN parties 
+  ON candidates.party_id = parties.id 
+  WHERE candidates.id = ?`;
     const params = [req.params.id];
   
     db.query(sql, params, (err, row) => {
